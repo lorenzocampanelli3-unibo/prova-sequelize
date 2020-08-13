1b5b6c9612dab9620a868634ed2114aab105368d
Lorenzo Campanelli <lorenzo.campanelli3@studio.unibo.it>
2020-08-13 09:57:41 +0200

    Add new queries in test.js

--- test.js
+++ test.js
@@ -3,9 +3,9 @@
 const db = require('./models/index');
 
 (async () => {
-    await db.sequelize.sync({force: true});
-    console.log('Tables created');
     try {
+        await db.sequelize.sync({force: true});
+        console.log('Tables created');
         await db.User.create({
             userId: 1,
             firstName: "Lillo"
@@ -14,6 +14,27 @@ const db = require('./models/index');
             {UserId: 1},
             {title: "pippopluto", author: 5, UserId: 1},
         ]);
+        const resultUser = await db.User.findAll({
+            where: {
+                userId: 1
+            },
+            include: db.Post
+        });
+        console.log(JSON.stringify(resultUser, null, 2));
+        const resultPosts = await db.Post.findAll({
+            include: db.User
+        });
+        console.log(JSON.stringify(resultPosts, null, 2));
+        const resultUser2 = await db.User.findOne({
+            where: {
+                userId: 1
+            }
+        });
+        // console.log(resultUser2);
+        // console.log(resultUser2.firstName);
+        console.log(JSON.stringify(resultUser2, null, 2));
+        const resultUser2Posts = await resultUser2.getPosts();
+        console.log(JSON.stringify(resultUser2Posts, null, 2));
     } catch (err) {
         console.log('error: ', err);
     }
